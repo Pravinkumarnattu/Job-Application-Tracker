e10e29a3648418c2b2f5fa019d1dd4d8743679cc
Pravin kumar <pravinkumarnattu@gmail.com>
2026-09-04 12:31:09 +0530

    GoogleAuth code exchange finished

--- server/src/controllers/googleAuth.js
+++ server/src/controllers/googleAuth.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const exchangeCodeForTokens = require("../services/googleOAuthService/exchangeCodeForTokens");
-const getGoogleUserProfile = require("../services/googleOAuthService/getGoogleUserProfile");
+const {
+  exchangeCodeForTokens,
+  getGoogleUserProfile,
+} = require("../services/googleOAuthService");
 
 const googleAuth = async (req, res) => {
   try {
@@ -9,29 +11,42 @@ const googleAuth = async (req, res) => {
     const tokens = await exchangeCodeForTokens(code);
     const { access_token, refresh_token } = tokens;
     const googleProfile = await getGoogleUserProfile(access_token);
-    const { email, googleId } = googleProfile;
+    const { email, id: googleId, name } = googleProfile;
     const existingUserByGoogleId = await User.findOne({ googleId });
-    const token = null;
+    let user;
     if (!existingUserByGoogleId) {
       const existingUserByEmail = await User.findOne({ email });
-      if (existingUserByEmail) {
-        existingUserByEmail.googleId = googleId;
-        existingUserByEmail.googleRefreshToken = refresh_token;
-        await existingUserByEmail.save();
-      } else {
-        const newUser = new User.create({
+      if (!existingUserByEmail) {
+        const newUser = new User({
           email,
           googleId,
+          name,
           authProvider: "google",
           googleRefreshToken: refresh_token,
         });
         await newUser.save();
+        user = newUser;
+      } else {
+        existingUserByEmail.googleId = googleId;
+        existingUserByEmail.googleRefreshToken = refresh_token;
+        await existingUserByEmail.save();
+        user = existingUserByEmail;
       }
     } else {
-      token = jwt.sign({ id: existingUserByGoogleId._id}, process.env.JWT_SECRET);
       existingUserByGoogleId.googleRefreshToken = refresh_token;
       await existingUserByGoogleId.save();
+      user = existingUserByGoogleId;
     }
-    
-  } catch (err) {}
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    return res.status(200).json({
+      token,
+      user: { email: user.email, googleId: user.googleId, name: user.name },
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
+
+module.exports = googleAuth;
